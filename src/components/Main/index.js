69032e4c3c510cb847127fda1e833113a3cb7ad7
BenPuusta <benpuusta@gmail.com>
2022-11-10 21:09:46 +0000

    bit of refactoring and other tasks

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -8,13 +8,12 @@ import Post from '../Post';
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
-  //posts state is above
-// console.log(posts)
 
   return <main id="main">
     {posts.map(
       (post)=> {
         return <Post 
+        image = {post.image}
         key = {post.postId}
         title = {post.title} 
         date ={post.date}
@@ -24,11 +23,6 @@ function Main() {
           console.log (post.image.link)
           return <li key={index} >{item}</li>
         })}
-        image = {post.image}
-        // image = {post.image.map((image) =>{ return <img src ={image.image.link} alt ={image.image.alt}></img>})} 
-
-       
-
    
         />
       }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -5,11 +5,13 @@ import './index.css';
 function Post({title, date, author, text, highlights, image}) {
   return <div>
     <p>{title}</p>
+    <div className = "pigeonContainer">
+    <img className = "picture" src = {image.link} img alt = {image.alt}></img></div>
     <p>{date}</p>
     <p>{author}</p>
     <article>{text}</article>
     <summary>{highlights}</summary>
-    <img src = {image.link} img alt = {image.alt}></img>
+
   </div>;
 }
 
