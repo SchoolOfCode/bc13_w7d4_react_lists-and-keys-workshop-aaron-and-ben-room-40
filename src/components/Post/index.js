54b8fc09b31ddeafb0596188b4fcd6d15431eb4e
BenPuusta <benpuusta@gmail.com>
2022-11-10 16:09:14 +0000

    tasks 1-3

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post({title, date, author, text, highlights,}) {
+  return <article>{title} {date}</article>;
 }
 
+
 export default Post;
+
+// const postTemplate = {
+//   postId: 0,
+//   title: 'a title',
+//   date: 'DD/MM/YY',
+//   author: 'author',
+//   text: lorem.generateParagraphs(7),
+//   highlights: [
+//     lorem.generateWords(3),
+//     lorem.generateWords(3),
+//     lorem.generateWords(3),
+//   ],
+//   image: { link: 'link', alt: 'alt' },
+// };
